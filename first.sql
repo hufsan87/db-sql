0b53c752d79304094eee8ad129e17c4426694893
이현석 <hslee@hist.co.kr>
2025-04-25 18:46:42 +0900

    2nd commit

--- first.sql
+++ first.sql
@@ -1,1 +1,22 @@
-select sysdate from dual;
+select sysdate-1/12 from dual;
+
+
+set serveroutput on;
+
+
+DECLARE
+    tt varchar2(100):='test한글나라';
+BEGIN
+    dbms_output.put_line('hello world');
+
+    dbms_output.put_line('tt : '||tt);
+
+    dbms_output.put_line('tt111 : '||tt);
+
+end;
+
+
+
+
+
+select * from thrm100 where enter_cd='HX';
