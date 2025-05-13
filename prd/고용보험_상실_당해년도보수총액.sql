4703fa8490d75fca8e2ce13bfb9e68be312bb28f
이현석 <hslee@hist.co.kr>
2025-05-13 14:59:03 +0900

    [한국공항] 고용보험 상실, 당해년도보수총액 수정

--- prd/고용보험_상실_당해년도보수총액.sql
+++ prd/고용보험_상실_당해년도보수총액.sql
@@ -1,5 +1,5 @@
-SELECT (                       
-        SELECT B.CURR_TOT_MON AS MON
+SELECT COALESCE(                       
+        (SELECT B.CURR_TOT_MON AS MON
           FROM TCPN811 A, TCPN841 B, THRM100 C
          WHERE A.ENTER_CD = @@회사코드@@
            AND A.SABUN = @@사번@@
@@ -10,14 +10,13 @@ SELECT (
            AND A.SABUN = B.SABUN
            AND A.ENTER_CD = C.ENTER_CD
            AND A.SABUN = C.SABUN
-           AND SUBSTR(NVL(RET_YMD, '29991231'),0,4) = A.WORK_YY
-        UNION ALL
-        SELECT SUM(NVL(A.DATA_1,0)+NVL(A.DATA_2,0)+NVL(A.DATA_3,0)+NVL(A.DATA_4,0)+NVL(A.DATA_5,0)+NVL(A.DATA_6,0)) AS MON
+           AND SUBSTR(NVL(RET_YMD, '29991231'),0,4) = A.WORK_YY),
+        (SELECT SUM(NVL(A.DATA_1,0)+NVL(A.DATA_2,0)+NVL(A.DATA_3,0)+NVL(A.DATA_4,0)+NVL(A.DATA_5,0)+NVL(A.DATA_6,0)) AS MON
           FROM TYEA003 A, THRM100 B
          WHERE A.ENTER_CD = B.ENTER_CD
            AND A.SABUN = B.SABUN
            AND A.ENTER_CD = @@회사코드@@
            AND A.WORK_YY = SUBSTR(NVL(B.RET_YMD, '29991231'),0,4)
            AND A.SABUN = @@사번@@
-           AND A.ADJUST_TYPE = '3'
-     ) FROM DUAL
+           AND A.ADJUST_TYPE = '3')
+     ) AS MON FROM DUAL
