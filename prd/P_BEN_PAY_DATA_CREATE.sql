a71ecc27b3b710eeb97e7cc486a6aa2bd30f815c
이현석 <hslee@hist.co.kr>
2025-05-16 17:11:19 +0900

    P_BEN_PAY_DATA_CREATE 건간보험, TP 추가사항 적용

--- prd/P_BEN_PAY_DATA_CREATE.sql
+++ prd/P_BEN_PAY_DATA_CREATE.sql
@@ -248,7 +248,7 @@ BEGIN
             /* -- 건강보험 급여공제방식(C:공단자료연계) */
             IF lv_ben_cal_type = 'C' THEN
                BEGIN
-                IF P_ENTER_CD = 'HX' THEN -- 한진정보통신, 공단자료(사번) 업로드 이용
+                IF P_ENTER_CD in ('HX','TP') THEN -- 한진정보통신, 공단자료(사번) 업로드 이용, (2025.05.16)TP 추가
                   INSERT INTO TBEN777
                   (
                           ENTER_CD --회사구분(TORG900)
