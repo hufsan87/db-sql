31c3b7d886d1ef77b0e6c810b490c5e5fb72a722
이현석 <hslee@hist.co.kr>
2025-07-31 19:13:10 +0900

    OTHER

--- dev/PKG_REPORT_VIEWS.sql
+++ dev/PKG_REPORT_VIEWS.sql
@@ -12,7 +12,6 @@ create or replace PACKAGE PKG_REPORT_VIEWS AS
         NAME            VARCHAR2(100), -- F_COM_GET_NAMES 반환값 크기
         EMP_YMD         VARCHAR2(8),
         ORG_CD          VARCHAR2(20),
-        WORK_ORG_CD     VARCHAR2(20),
         JIKGUB_NM       VARCHAR2(100), -- F_COM_GET_GRCODE_NAME 반환값 크기
         ORG_NM          VARCHAR2(100), -- F_COM_GET_ORG_NM 반환값 크기
         P_ORG_NM        VARCHAR2(100), -- F_COM_GET_PRIOR_ORG_TYPE_NM 반환값 크기
@@ -21,6 +20,7 @@ create or replace PACKAGE PKG_REPORT_VIEWS AS
         HOL_YN          VARCHAR2(1),
         GNT_CD          VARCHAR2(50),
         GNT_NM          VARCHAR2(100),
+        ORD_E_YMD       VARCHAR2(8),
         WORK_CD         VARCHAR2(50),
         FLEX_YN         VARCHAR2(1),
         SDATE           VARCHAR2(8), -- TTIM131.SDATE
@@ -54,32 +54,62 @@ create or replace PACKAGE BODY PKG_REPORT_VIEWS AS
     ) RETURN TY_REPORT_VIEW_TABLE PIPELINED
     AS
         V_REC TY_REPORT_VIEW_REC;
-
     BEGIN
         FOR REC IN (
             WITH TMP AS (
-                SELECT A.ENTER_CD, B.STATUS_CD
+/*                SELECT A.ENTER_CD, B.STATUS_CD
                      , A.SABUN
                      , F_COM_GET_NAMES(A.ENTER_CD, A.SABUN) AS NAME
                      , A.EMP_YMD
-                     , NVL(C.ORG_CD, B.ORG_CD) AS ORG_CD
-                     , C.WORK_ORG_CD
+                     , B.ORG_CD
                      , F_COM_GET_GRCODE_NAME(B.ENTER_CD, 'H20010', B.JIKGUB_CD) AS JIKGUB_NM
-                     , F_COM_GET_ORG_NM(B.ENTER_CD, NVL(C.ORG_CD, B.ORG_CD), P_BASE_YMD) AS ORG_NM
-                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_NM(B.ENTER_CD, NVL(C.ORG_CD, B.ORG_CD), 'B0400', P_BASE_YMD)
-                           , F_COM_GET_ORG_NM(B.ENTER_CD, NVL(C.ORG_CD, B.ORG_CD), P_BASE_YMD)) AS P_ORG_NM
-                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_CD(B.ENTER_CD, NVL(C.ORG_CD, B.ORG_CD), 'B0400', P_BASE_YMD)
+                     , F_COM_GET_ORG_NM(B.ENTER_CD, B.ORG_CD, P_BASE_YMD) AS ORG_NM
+                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_NM(B.ENTER_CD, B.ORG_CD, 'B0400', P_BASE_YMD)
+                           , F_COM_GET_ORG_NM(B.ENTER_CD, B.ORG_CD, P_BASE_YMD)) AS P_ORG_NM
+                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_CD(B.ENTER_CD, B.ORG_CD, 'B0400', P_BASE_YMD)
                            , B.ORG_CD) AS P_ORG_CD
                      , '01' AS "01"
-                  FROM THRM100 A, THRM151 B, TTIM111_V C
+                  FROM THRM100 A, THRM151 B
                  WHERE A.ENTER_CD = P_ENTER_CD
                    AND A.ENTER_CD = B.ENTER_CD
                    AND A.SABUN    = B.SABUN
                    AND A.SABUN IN (SELECT DISTINCT SABUN FROM THRM151_AUTH(P_ENTER_CD, 'A', P_SEARCH_SABUN, '10')) --SEARCH_TYPE 조회구분(자신만조회:P, 권한범위적용:O, 전사:A), AUTH_GRP 권한그룹 10 관리자
                    AND P_BASE_YMD BETWEEN B.SDATE AND NVL(B.EDATE, '99991231')
-                   AND A.ENTER_CD = C.ENTER_CD(+)
-                   AND A.SABUN    = C.SABUN(+)
-                   AND P_BASE_YMD BETWEEN C.SDATE(+) AND NVL(C.EDATE(+), '99991231')
+                   AND B.STATUS_CD NOT LIKE 'RA%' -- 퇴직자 제외(RAA?)
+                   AND (A.RET_YMD IS NULL OR A.RET_YMD = P_BASE_YMD) --당일 퇴직자만 표출
+                   AND B.ORG_CD = NVL(P_ORG_CD_2, B.ORG_CD) --조직, 없으면 전체*/
+                   
+                SELECT A.ENTER_CD, B.STATUS_CD
+                     , A.SABUN
+                     , F_COM_GET_NAMES(A.ENTER_CD, A.SABUN) AS NAME
+                     , A.EMP_YMD
+                     , B.ORG_CD
+                     , F_COM_GET_GRCODE_NAME(B.ENTER_CD, 'H20010', B.JIKGUB_CD) AS JIKGUB_NM
+                     , F_COM_GET_ORG_NM(B.ENTER_CD, B.ORG_CD, P_BASE_YMD) AS ORG_NM
+                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_NM(B.ENTER_CD, B.ORG_CD, 'B0400', P_BASE_YMD)
+                           , F_COM_GET_ORG_NM(B.ENTER_CD, B.ORG_CD, P_BASE_YMD)) AS P_ORG_NM
+                     , NVL(F_COM_GET_PRIOR_ORG_TYPE_CD(B.ENTER_CD, B.ORG_CD, 'B0400', P_BASE_YMD)
+                           , B.ORG_CD) AS P_ORG_CD
+                     , '01' AS "01"
+					 , T191.ORD_E_YMD
+                  FROM THRM100 A
+						JOIN THRM151 B ON A.ENTER_CD = B.ENTER_CD AND A.SABUN = B.SABUN
+						LEFT OUTER JOIN (
+								  SELECT
+										ENTER_CD, SABUN, ORD_E_YMD,
+										ROW_NUMBER() OVER (PARTITION BY ENTER_CD, SABUN ORDER BY ORD_YMD DESC, APPLY_SEQ DESC) as rn
+								  FROM THRM191
+								  WHERE ORD_YMD <= P_BASE_YMD
+                                    AND (STATUS_CD != 'CA' OR (STATUS_CD = 'CA' AND ORD_E_YMD IS NOT NULL)) -- 조건 추가
+						) T191
+						  ON A.ENTER_CD = T191.ENTER_CD
+						  AND A.SABUN = T191.SABUN
+						  AND T191.rn = 1
+                  WHERE A.ENTER_CD = P_ENTER_CD
+                   AND A.ENTER_CD = B.ENTER_CD
+                   AND A.SABUN    = B.SABUN
+                   AND A.SABUN IN (SELECT DISTINCT SABUN FROM THRM151_AUTH(P_ENTER_CD, 'A', P_SEARCH_SABUN, '10')) --SEARCH_TYPE 조회구분(자신만조회:P, 권한범위적용:O, 전사:A), AUTH_GRP 권한그룹 10 관리자
+                   AND P_BASE_YMD BETWEEN B.SDATE AND NVL(B.EDATE, '99991231')
                    AND B.STATUS_CD NOT LIKE 'RA%' -- 퇴직자 제외(RAA?)
                    AND (A.RET_YMD IS NULL OR A.RET_YMD = P_BASE_YMD) --당일 퇴직자만 표출
                    AND B.ORG_CD = NVL(P_ORG_CD_2, B.ORG_CD) --조직, 없으면 전체
@@ -92,7 +122,6 @@ create or replace PACKAGE BODY PKG_REPORT_VIEWS AS
                  , A.NAME
                  , A.EMP_YMD
                  , A.ORG_CD
-                 , A.WORK_ORG_CD
                  , A.JIKGUB_NM
                  , A.ORG_NM
                  , A.P_ORG_NM
@@ -101,6 +130,7 @@ create or replace PACKAGE BODY PKG_REPORT_VIEWS AS
                  , F_COM_GET_HOL_YN(A.ENTER_CD, P_BASE_YMD) HOL_YN
                  , F_TIM_GET_DAY_GNT_CD(A.ENTER_CD, A.SABUN, P_BASE_YMD) GNT_CD
                  , F_TIM_GET_DAY_GNT_NM(A.ENTER_CD, A.SABUN, P_BASE_YMD) GNT_NM
+                 , A.ORD_E_YMD
                  , F_TIM_GET_DAY_WORK_CD(A.ENTER_CD, A.SABUN, P_BASE_YMD) WORK_CD
                  , F_TIM_AGILE_SCHEDULE_YN(A.ENTER_CD, A.SABUN, P_BASE_YMD, P_BASE_YMD) FLEX_YN
                  , T9.SDATE
@@ -127,7 +157,10 @@ create or replace PACKAGE BODY PKG_REPORT_VIEWS AS
                         T2.EDATE,
                         ROW_NUMBER() OVER (PARTITION BY T2.ENTER_CD, T2.SABUN, T2.SDATE, T2.EDATE ORDER BY T2.APPL_SEQ DESC) as rn
                     FROM TTIM131 T2
-                    WHERE 1 = (SELECT 1 FROM THRI103 Z WHERE Z.ENTER_CD=T2.ENTER_CD AND Z.APPL_SEQ=T2.APPL_SEQ AND Z.APPL_STATUS_CD='99')
+                    --WHERE 1 = (SELECT 1 FROM THRI103 Z WHERE Z.ENTER_CD=T2.ENTER_CD AND Z.APPL_SEQ=T2.APPL_SEQ AND Z.APPL_STATUS_CD='99')
+                    JOIN THRI103 Z 
+                        ON Z.ENTER_CD = T2.ENTER_CD AND Z.APPL_SEQ = T2.APPL_SEQ
+                        WHERE Z.APPL_STATUS_CD = '99'
                 ) T3 ON T1.ENTER_CD = T3.ENTER_CD
                 AND T1.SABUN = T3.SABUN
                 AND T1.YMD BETWEEN T3.SDATE AND T3.EDATE
@@ -149,15 +182,15 @@ create or replace PACKAGE BODY PKG_REPORT_VIEWS AS
             V_REC.NAME           := REC.NAME;
             V_REC.EMP_YMD        := REC.EMP_YMD;
             V_REC.ORG_CD         := REC.ORG_CD;
-            V_REC.WORK_ORG_CD    := REC.WORK_ORG_CD;
             V_REC.JIKGUB_NM      := REC.JIKGUB_NM;
             V_REC.ORG_NM         := REC.ORG_NM;
             V_REC.P_ORG_NM       := REC.P_ORG_NM;
-            V_REC.P_ORG_CD   := REC.P_ORG_CD;
+            V_REC.P_ORG_CD       := REC.P_ORG_CD;
             V_REC.HOUR_0_0       := REC.HOUR_0_0;
             V_REC.HOL_YN         := REC.HOL_YN;
             V_REC.GNT_CD         := REC.GNT_CD;
             V_REC.GNT_NM         := REC.GNT_NM;
+            V_REC.ORD_E_YMD      := REC.ORD_E_YMD;
             V_REC.WORK_CD        := REC.WORK_CD;
             V_REC.FLEX_YN        := REC.FLEX_YN;
             V_REC.SDATE          := REC.SDATE;
