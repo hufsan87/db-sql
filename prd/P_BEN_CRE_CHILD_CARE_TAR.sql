6b983be898507748cef5a82ce7231b99d216dfa1
이현석 <hslee@hist.co.kr>
2025-10-20 15:45:17 +0900

    OTHER

--- prd/P_BEN_CRE_CHILD_CARE_TAR.sql
+++ prd/P_BEN_CRE_CHILD_CARE_TAR.sql
@@ -402,7 +402,9 @@ BEGIN
                         --AND (A.USE_S_YM <= P_TAR_YM AND P_TAR_YM < NVL(A.USE_E_YM,'99991231'))
                         --AND (A.PAY_STS = 'P' OR ( A.PAY_STS = 'S' AND ( NVL(A.USE_MS_YM, A.USE_S_YM) <= P_TAR_YM AND P_TAR_YM < NVL(A.USE_E_YM,A.USE_M_YM))))
                         -- 추가 '25.05.29
-                        AND P_TAR_YM BETWEEN A.USE_S_YM AND NVL(A.USE_E_YM, '99991231')
+                        --AND P_TAR_YM BETWEEN A.USE_S_YM AND NVL(A.USE_E_YM, '999912')
+                        --종료년월 부터, 대상에서 제외되어야 함 25.10.20
+                        AND P_TAR_YM BETWEEN A.USE_S_YM AND TO_CHAR(ADD_MONTHS(TO_DATE(NVL(A.USE_E_YM, '999912')||'01','YYYYMMDD'), -1),'YYYYMM') 
                         AND (A.USE_M_YM IS NULL 
                              OR (
                                A.USE_M_YM>P_TAR_YM
